88e2197eab6a98bfe91c177d1edb917838a9919f
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-05-20 12:24:22 -0400

    Update singularity script

--- singularity/singularity.sh
+++ singularity/singularity.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
-export VERSION=3.4.2
+VERSION=3.5.3
+NAME=$(echo $VERSION | cut -d. -f1,2)
 curl -L -O https://github.com/sylabs/singularity/releases/download/v$VERSION/singularity-$VERSION.tar.gz
 tar xf singularity-${VERSION}.tar.gz singularity/singularity.spec
 mv singularity/singularity.spec .
-sed -i '28 a %global _prefix /opt/software/singularity-3.4' singularity.spec
+sed -i "28 a %global _prefix /opt/software/singularity-${NAME}" singularity.spec
