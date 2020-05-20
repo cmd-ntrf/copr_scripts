3c5775ecf5cdbfb6b36056070b285ec3c46c5927
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-05-20 13:52:57 -0400

    Update singularity script

--- singularity/singularity.sh
+++ singularity/singularity.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -x
-VERSION=3.5.3
+
+TAG=$(curl -s -N https://api.github.com/repos/hpcng/singularity/tags | jq -r '.[].name' | grep -m 1 -v rc)
+VERSION=$(echo $TAG | sed 's/^v//')
 NAME=$(echo $VERSION | cut -d. -f1,2)
-curl -L -O https://github.com/sylabs/singularity/releases/download/v$VERSION/singularity-$VERSION.tar.gz
+curl -L -O https://github.com/hpcng/singularity/releases/download/${TAG}/singularity-$VERSION.tar.gz
 tar xf singularity-${VERSION}.tar.gz singularity/singularity.spec
 mv singularity/singularity.spec .
 sed -i "28 a %global _prefix /opt/software/singularity-${NAME}" singularity.spec
