b61871b809bcbc76af3435d490cfc29d4ba9ff58
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-05-24 00:21:44 -0400

    Change tar source for slurm19

--- slurm19/slurm19.sh
+++ slurm19/slurm19.sh
@@ -6,19 +6,17 @@ VERSION=$(echo $TAG | cut -d- -f2,3,4,5)
 TARNAME=$(echo $VERSION | sed 's/-/./g' | cut -d. -f1,2,3)
 
 curl -L -O https://raw.githubusercontent.com/SchedMD/slurm/slurm-${VERSION}/slurm.spec
-sed -i 's/.bz2/.gz/g' slurm.spec
 sed -i '/^%configure/a \ \ \ \ \ \ \ \ --prefix=/opt/software/slurm \\' slurm.spec
 sed -i 's/python$/python3/g' slurm.spec
 sed -i '9 a %global _prefix /opt/software/slurm' slurm.spec
-curl -L -O https://github.com/SchedMD/slurm/archive/slurm-${VERSION}.tar.gz
-tar xvf slurm-${VERSION}.tar.gz
-rm slurm-${VERSION}.tar.gz
-mv slurm-slurm-${VERSION} slurm-${TARNAME}
+curl -L -O https://download.schedmd.com/slurm/slurm-${TARNAME}.tar.bz2
+tar xf slurm-${TARNAME}.tar.bz2
+rm slurm-${TARNAME}.tar.bz2
 cd slurm-${TARNAME}
 sed -i -e 's;nvml_includes=".*";nvml_includes="-I/usr/local/cuda-10.1/targets/x86_64-linux/include";g' auxdir/x_ac_nvml.m4
 sed -i -e 's;#!/usr/bin/env python;#!/usr/bin/env python3;g' doc/html/shtml2html.py
 autoconf
 automake
 cd ..
-tar czvf slurm-${TARNAME}.tar.gz slurm-${TARNAME}
+tar cjSf slurm-${TARNAME}.tar.bz2 slurm-${TARNAME}
 rm -rf slurm-${TARNAME}
