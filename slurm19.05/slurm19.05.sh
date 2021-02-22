2ff9b1d246134bd72e4ae70d08bbfd24f2665594
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-02-22 15:12:20 -0500

    Rename slurm19 to slurm19.05 and slurm20 to slurm20.11

--- slurm19.05/slurm19.05.sh
+++ slurm19.05/slurm19.05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-TAG=$(curl -s -N https://api.github.com/repos/schedmd/slurm/tags | jq -r '.[].name' | grep -m 1 slurm-19)
+TAG=$(curl -s -N https://api.github.com/repos/schedmd/slurm/tags | jq -r '.[].name' | grep -m 1 slurm-19-05)
 VERSION=$(echo $TAG | cut -d- -f2,3,4,5)
 TARNAME=$(echo $VERSION | sed 's/-/./g' | cut -d. -f1,2,3)
 
