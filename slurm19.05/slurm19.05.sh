0b209b7e8697fcbbf4603e5c1f8b9c2174b66125
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-02-22 15:36:46 -0500

    Fix url to patches

--- slurm19.05/slurm19.05.sh
+++ slurm19.05/slurm19.05.sh
@@ -6,7 +6,7 @@ VERSION=$(echo $TAG | cut -d- -f2,3,4,5)
 TARNAME=$(echo $VERSION | sed 's/-/./g' | cut -d. -f1,2,3)
 
 curl -L -O https://raw.githubusercontent.com/SchedMD/slurm/slurm-${VERSION}/slurm.spec
-curl https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/slurm19/1be5492.patch | patch -p1
+curl https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/slurm19.05/1be5492.patch | patch -p1
 sed -i '/^%configure/a \ \ \ \ \ \ \ \ --prefix=/opt/software/slurm \\' slurm.spec
 sed -i 's/python$/python3/g' slurm.spec
 sed -i '9 a %global _prefix /opt/software/slurm' slurm.spec
@@ -22,7 +22,7 @@ rm slurm-${TARNAME}.tar.bz2
 cd slurm-${TARNAME}
 sed -i -e 's;nvml_includes=".*";nvml_includes="-I/usr/local/cuda-11.1/targets/x86_64-linux/include";g' auxdir/x_ac_nvml.m4
 sed -i -e 's;#!/usr/bin/env python;#!/usr/bin/env python3;g' doc/html/shtml2html.py
-curl https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/slurm19/bug9109.patch | patch -p1
+curl https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/slurm19.05/bug9109.patch | patch -p1
 autoconf
 aclocal
 automake
