5ed59d3489ea84fded95ba40d4f63a3c42cdafb0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-26 08:55:27 -0400

    Set mokey version to v0.5.5

--- mokey/mokey.sh
+++ mokey/mokey.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
-curl -L -O https://github.com/ubccr/mokey/archive/master.zip
+VERSION=0.5.5
+
+curl -L -O https://github.com/ubccr/mokey/archive/v${VERSION}.zip
 curl -L -O https://golang.org/dl/go1.13.15.linux-amd64.tar.gz
 
 tar xvf go*.linux-amd64.tar.gz
 export PATH=$PATH:$PWD/go/bin
 
-unzip master.zip
-cd mokey-master
+unzip mokey-${VERSION}.zip
+cd mokey-${VERSION}
 go build .
 . scripts/make-release.sh
 cd ..
 
-cp mokey-master/mokey.spec .
-cp mokey-master/*linux-amd64.tar.gz .
+cp mokey-${VERSION}/mokey.spec .
+cp mokey-${VERSION}/*linux-amd64.tar.gz .
 
-rm -rf go/ go1.13.15.linux-amd64.tar.gz master.zip mokey-master/
+rm -rf go/ go1.13.15.linux-amd64.tar.gz v${VERSION}.zip mokey-${VERSION}/
