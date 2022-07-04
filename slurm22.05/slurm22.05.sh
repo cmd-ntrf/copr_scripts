536480b8d1cf11b8dee37fc3c020dc83a1b9436a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-07-04 09:24:29 -0400

    Add script for slurm-22.05

---
+++ slurm22.05/slurm22.05.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+TAG=$(curl -s -N https://api.github.com/repos/schedmd/slurm/tags | jq -r '.[].name' | grep -m 1 slurm-22-05)
+VERSION=$(echo $TAG | cut -d- -f2,3,4,5)
+TARNAME=$(echo $VERSION | sed 's/-/./g' | cut -d. -f1,2,3)
+
+curl -L -O https://raw.githubusercontent.com/SchedMD/slurm/slurm-${VERSION}/slurm.spec
+sed -i '/^%configure/a \ \ \ \ \ \ \ \ --enable-selinux \\' slurm.spec
+sed -i '/^%configure/a \ \ \ \ \ \ \ \ --prefix=/opt/software/slurm \\' slurm.spec
+# auth_jwt.so is missing in Slurm 20.11.7
+sed -i '/^%files slurmrestd/a %{_libdir}/slurm/auth_jwt.so' slurm.spec
+sed -i '9 a %global _prefix /opt/software/slurm' slurm.spec
+curl -L -O https://download.schedmd.com/slurm/slurm-${TARNAME}.tar.bz2
+tar xf slurm-${TARNAME}.tar.bz2
+rm slurm-${TARNAME}.tar.bz2
+cd slurm-${TARNAME}
+sed -i -e "s;_x_ac_nvml_dirs=\".*\";_x_ac_nvml_dirs=\"$(echo /usr/local/cuda-11.*)\";g" auxdir/x_ac_nvml.m4
+curl https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/slurm22.05/bug9109.patch | patch -p1
+autoconf
+aclocal
+automake
+cd ..
+tar cjSf slurm-${TARNAME}.tar.bz2 slurm-${TARNAME}
+rm -rf slurm-${TARNAME}
