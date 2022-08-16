219ef222336f5d3013970f974c400c1dc19d2a06
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-08-16 11:11:47 -0400

    Fix tarname for slurm21.08
    The release version is greater than 1, so it's included in the
    expected tarname.

--- slurm21.08/slurm21.08.sh
+++ slurm21.08/slurm21.08.sh
@@ -3,7 +3,7 @@ set -e
 
 TAG=$(curl -s -N https://api.github.com/repos/schedmd/slurm/tags | jq -r '.[].name' | grep -m 1 slurm-21-08)
 VERSION=$(echo $TAG | cut -d- -f2,3,4,5)
-TARNAME=$(echo $VERSION | sed 's/-/./g' | cut -d. -f1,2,3)
+TARNAME=$(echo $VERSION | sed 's/-/./1' | sed 's/-/./1')
 
 curl -L -O https://raw.githubusercontent.com/SchedMD/slurm/slurm-${VERSION}/slurm.spec
 sed -i '/^%configure/a \ \ \ \ \ \ \ \ --enable-selinux \\' slurm.spec
