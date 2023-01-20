0ccaafeabf69ebccc53d9b393e47f8fb8ec3079b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-01-20 12:29:12 -0500

    Define arch in mokey script

--- mokey/mokey.sh
+++ mokey/mokey.sh
@@ -2,16 +2,22 @@
 set -e
 
 VERSION=0.5.6
+GOVERSION=1.13.15
+if [ $(uname -m) == "x86_64" ]; then
+    ARCH=amd64
+else
+    ARCH=arm64
+fi
 
 curl -L -O https://github.com/ubccr/mokey/archive/v${VERSION}.zip
-curl -L -O https://golang.org/dl/go1.13.15.linux-amd64.tar.gz
+curl -L -O https://golang.org/dl/go${GOVERSION}.linux-${ARCH}.tar.gz
 
-tar xvf go*.linux-amd64.tar.gz
+tar xvf go*.linux-${ARCH}.tar.gz
 export PATH=$PATH:$PWD/go/bin
 
 unzip v${VERSION}.zip
 cd mokey-${VERSION}
-go build .
+GOARCH=${ARCH} go build .
 . scripts/make-release.sh
 cd ..
 
