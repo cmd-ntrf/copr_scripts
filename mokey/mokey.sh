72e176ee10ec25f1fcbf6ab2bc2623088d3da2b1
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-01-20 13:00:40 -0500

    Replace mokey spec to use go build

--- mokey/mokey.sh
+++ mokey/mokey.sh
@@ -2,26 +2,6 @@
 set -e
 
 VERSION=0.5.6
-GOVERSION=1.13.15
-if [ $(uname -m) == "x86_64" ]; then
-    ARCH=amd64
-else
-    ARCH=arm64
-fi
+curl -L https://github.com/ubccr/mokey/archive/v${VERSION}.tar.gz -o mokey-${VERSION}.tar.gz
+curl -L -O https://raw.githubusercontent.com/cmd-ntrf/copr_scripts/master/mokey/mokey.spec
 
-curl -L -O https://github.com/ubccr/mokey/archive/v${VERSION}.zip
-curl -L -O https://golang.org/dl/go${GOVERSION}.linux-${ARCH}.tar.gz
-
-tar xvf go*.linux-${ARCH}.tar.gz
-export PATH=$PATH:$PWD/go/bin
-
-unzip v${VERSION}.zip
-cd mokey-${VERSION}
-GOARCH=${ARCH} go build .
-. scripts/make-release.sh
-cd ..
-
-cp mokey-${VERSION}/mokey.spec .
-cp mokey-${VERSION}/*linux-amd64.tar.gz .
-
-rm -rf go/ go1.13.15.linux-amd64.tar.gz v${VERSION}.zip mokey-${VERSION}/
