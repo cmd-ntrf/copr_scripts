aaeb793fec7f34014f5b7cc3a442331e2848d08b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-07-07 08:41:55 -0400

    Fix

--- slurm26.05/slurm26.05.sh
+++ slurm26.05/slurm26.05.sh
@@ -22,8 +22,7 @@ sed -i -e "s;QA_RPATHS=0x5;QA_RPATHS=0x7;g" slurm.spec
 curl -L -O https://github.com/MagicCastle/slurm-select-cons_tres_cloud/archive/refs/heads/patches.zip
 unzip patches.zip
 mv slurm-select-cons_tres_cloud-patches cons_tres_cloud
-rm patches.zip
-sed -i "9 a %global patch $(ls cons_tres_cloud/patches/${SLURM_VERSION})" slurm.spec
+sed -i "9 a %global patch $(ls cons_tres_cloud/patches/${SLURM_VERSION} | xargs echo)" slurm.spec
 
 curl -L -O https://download.schedmd.com/slurm/${TARNAME}
 tar xf ${TARNAME}
@@ -37,3 +36,4 @@ cd ..
 tar cjSf ${TARNAME} ${DIRNAME}
 rm -rf ${DIRNAME}
 rm -rf cons_tres_cloud
+rm patches.zip
